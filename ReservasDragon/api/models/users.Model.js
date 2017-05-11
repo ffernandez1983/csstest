aaf76ec41c048ed01c7071073240fb3409e76e5d
ffernandez1983 <ffernandez1983@gmail.com>
2017-05-11 09:52:29 +0200

    crud

--- ReservasDragon/api/models/users.Model.js
+++ ReservasDragon/api/models/users.Model.js
@@ -29,4 +29,74 @@ myuser.getUsers = function(callback)
 		});
 	}
 }
+
+myuser.postUsers = function(user, callback)
+{
+	if (connection) 
+	{
+		connection.query('INSERT INTO Usuarios SET ?', user 
+			,function(error, response) {
+				if(error)
+				{
+					throw error;
+				}
+				else
+				{
+					callback(null,response);
+				}
+			});
+	}
+}
+
+myuser.putUsers = function(id, user, callback)
+{
+	if (connection) 
+	{
+		var myQuery = 'UPDATE Usuarios SET Email = '+ connection.escape(user.Email) 
+		+ ',' + 'Telefono = ' + connection.escape(user.Telefono) 
+		+ 'WHERE ID_Usuario =' + id;
+		connection.query(myQuery
+			,function(error, response) {
+				if(error)
+				{
+					throw error;
+				}
+				else
+				{
+					callback(null,response);
+				}
+			});
+	}
+}
+
+myuser.deleteUsers = function(id, callback)
+{
+	if (connection) 
+	{
+		var sqlExists = 'SELECT * FROM Usuarios WHERE ID_Usuario = ' + id;
+		connection.query(sqlExists, function(err, row) 
+		{
+			//si existe la id del usuario a eliminar
+			if(row)
+			{
+				var sql = 'DELETE FROM Usuarios WHERE ID_Usuario = ' + id;
+				connection.query(sql, function(error, result) 
+				{
+					if(error)
+					{
+						throw error;
+					}
+					else
+					{
+						callback(null,result);
+					}
+				});
+			}
+			else
+			{
+				callback(null,{"msg":"notExist"});
+			}
+		});
+	}
+}
 module.exports = myuser;
--- ReservasDragon/api/routes/users.Routes.js
+++ ReservasDragon/api/routes/users.Routes.js
@@ -7,14 +7,10 @@ router.get('/', function(req, res, next) {
 
   UserModel.getUsers(function(error, data)
 			{
-				//si existe el usuario mostramos el formulario
+				//respuesta json
 				if (typeof data !== 'undefined' && data.length > 0)
 				{
-					res.render("index",{ 
-						title : "Formulario", 
-						info : data,
-						length: data.length
-					});
+					res.json(data);
 				}
 				//en otro caso mostramos un error
 				else
@@ -22,6 +18,74 @@ router.get('/', function(req, res, next) {
 					res.json(404,{"msg":"notExist"});
 				}
 			});    
+});
+
+/* POST a NEW user */
+router.post('/', function(req, res, next) {
+
+  UserModel.postUsers(req.body,function(error, data)
+			{
+				//respuesta json
+				if ((data.affectedRows > 0 ) && (data.serverStatus == 2) && (data.error == null))
+				{
+					res.json({
+							"status":"OK",
+							"msg":data.message,
+							"id": data.insertId});
+				}
+				//en otro caso mostramos un error
+				else
+				{
+					res.json(412,{"msg":"error"});
+				}
+			});    
+});
+
+/* PUT / UPDATE Existing user */
+router.put('/:ID_Usuario', function(req, res, next) {
+	var id = req.params.ID_Usuario;
+	if(!isNaN(id)){
+		UserModel.putUsers(id, req.body,function(error, data)
+		{
+			//respuesta json
+			if ((data.error == null) && (data.serverStatus==2) && (data.affectedRows>0))
+			{
+				res.json({
+				"status":"OK",
+				"msg":data.message,
+				"id": id});
+			}
+
+			//en otro caso mostramos un error
+			else
+			{
+				res.json(404,{"msg":"notExist"});
+			}
+		});    
+	}
+});
+
+/* DELETE Existing user */
+router.delete('/:ID_Usuario', function(req, res, next) {
+var id = req.params.ID_Usuario;
+	if(!isNaN(id)){
+		UserModel.deleteUsers(id,function(error, data)
+		{
+			//respuesta json
+			if ((data.error == null) && (data.serverStatus==2) && (data.affectedRows>0))
+			{
+				res.json({
+						"status":"OK",
+						"msg": "deleted!",
+						"id": id});
+			}
+			//en otro caso mostramos un error
+			else
+			{
+				res.json(404,{"msg":"notExist"});
+			}
+		});    
+	}
 });
 
 module.exports = router;
